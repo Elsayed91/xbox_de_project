70a6d2cfbc11c961b9662d1d854cf8eb0428383e
Liftingthedata <liftingthedata@gmail.com>
2023-03-03 12:05:47 +0200

    e76f1ab589

--- airflow/dags/metacritic_vgchartz_dag.py
+++ airflow/dags/metacritic_vgchartz_dag.py
@@ -100,7 +100,7 @@ with DAG(
                 "console": console,
             }
         )
-        with TaskGroup(group_id='process-metacritic-data') as tg1:
+        with TaskGroup(group_id=f'process-{console}-data') as tg1:
             t2 = KubernetesJobOperator(
                 task_id=f"scrape-{console}-game-data",
                 body_filepath=POD_TEMPALTE,
