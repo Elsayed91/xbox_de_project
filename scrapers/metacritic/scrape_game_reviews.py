abb8f29f715cf9c913d898ac7f17b95a6f066334
Liftingthedata <liftingthedata@gmail.com>
2023-03-07 22:58:27 +0200

    f4f699df7d

--- scrapers/metacritic/scrape_game_reviews.py
+++ scrapers/metacritic/scrape_game_reviews.py
@@ -185,54 +185,6 @@ def scrape_user_reviews(
         exception_list.append(f"On game link {game_url}, Error : {e}")
 
 
-# def scrape_user_reviews(
-#     game_link: str, review_list: list, exception_list: list
-# ) -> None:
-#     """
-#     Scrapes user reviews for a game from Metacritic and appends them to the review_list.
-
-#     Args:
-#         game_link (str): The Metacritic URL for the game.
-#         review_list (list): The list to which the user reviews will be appended.
-#         exception_list (list): The list to which exceptions will be appended if they
-#         occur.
-
-#     Returns:
-#         None
-#     """
-#     try:
-#         url = game_link + "/user-reviews?page="
-#         pages = get_last_page_num(url)
-#         for page in range(pages):
-#             game_url = url + str(page)
-
-#             soup = soup_it(game_url)
-#             print(f"processing user review for {game_url}")
-#             game = soup.find("div", class_="product_title").find("h1").text.strip()
-#             platform = soup.find("span", class_="platform").text.strip()
-#             for review in soup.find_all("div", class_="review_content"):
-#                 if review.find("div", class_="name") == None:
-#                     break
-#                 review_list.append(
-#                     {
-#                         "Game": game,
-#                         "Platform": platform,
-#                         "User": review.find("div", class_="name").find("a").text,
-#                         "Date": review.find("div", class_="date").text,
-#                         "Score": review.find("div", class_="review_grade")
-#                         .find_all("div")[0]
-#                         .text,
-#                         "Review": review.find(
-#                             "span", class_="blurb blurb_expanded"
-#                         ).text
-#                         if review.find("span", class_="blurb blurb_expanded")
-#                         else review.find("div", class_="review_body").find("span").text,
-#                     }
-#                 )
-#     except BaseException as e:
-#         exception_list.append(f"On game link {game_url}, Error : {e}")
-
-
 def main(console: str, review_type: str, save_path: str) -> None:
     """
     Main function that scrapes either user reviews or critic reviews for the games in
@@ -262,6 +214,9 @@ def main(console: str, review_type: str, save_path: str) -> None:
     for game in game_list:
         print(f"processing {review_type} reviews for {game}")
         scrape_reviews(game, data_list, exception_list)
+        if game == "https://www.metacritic.com/game/xbox-series-x/elden-ring":
+            print("elden ring is processing")
+            print(data_list)
 
     df = pd.DataFrame.from_dict(data_list)
     df.to_parquet(f"{save_path}/{file_name}.parquet")
