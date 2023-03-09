fbdf444b685f48c1bfe6019a33ac2186ec326924
Liftingthedata <liftingthedata@gmail.com>
2023-03-09 15:49:44 +0200

    530c000a49

--- scrapers/metacritic/scrape_game_data.py
+++ scrapers/metacritic/scrape_game_data.py
@@ -96,11 +96,7 @@ def add_gamepass_status(main_df: pd.DataFrame) -> pd.DataFrame:
 
 
 def scrape_game_data(
-    link: str,
-    data_list: list[dict],
-    exception_list: list[str],
-    retry_late_list: list[str],
-    failed_links_list: list[str],
+    link: str, data_list: list[dict], exception_list: list[str]
 ) -> None:
     """
     Given a link, appends scraped data to a list of dictionaries representing game data
@@ -114,73 +110,81 @@ def scrape_game_data(
     Returns:
         None
     """
-    MAX_RETRIES = 2
-    retries = 0
-    while True:
+    try:
+        game_sublink = link.replace("https://www.metacritic.com", "")
+
+        MAX_RETRIES = 3
+
+        retries = 0
+        while True:
+            try:
+                soup = soup_it(link)
+                data = json.loads(soup.find("script", type="application/ld+json").text)
+                break
+            except Exception as e:
+                retries += 1
+                if retries >= MAX_RETRIES:
+                    print(
+                        f"Failed to scrape data from {link} after {MAX_RETRIES} retries."
+                    )
+                    exception_list.append(f"On game link {link}, Error : {e}")
+                    return
+                else:
+                    print(f"Retrying {link}...")
+                    time.sleep(1)
+                    continue
         try:
-            soup = soup_it(link)
-            data = json.loads(soup.find("script", type="application/ld+json").text)
-            break
-        except Exception as e:
-            retries += 1
-            if retries >= MAX_RETRIES:
-                print(f"Failed to scrape data from {link} after {MAX_RETRIES} retries.")
-                failed_links_list.append(link)
-                return
-            else:
-                print(f"Retrying {link}...")
-                time.sleep(5)
-                continue
-    try:
-        user_score = soup.find("div", class_="user").text
-        user_score = float(user_score) if user_score != "tbd" else None
-    except:
-        user_score = None
+            user_score = soup.find("div", class_="user").text
+            user_score = float(user_score) if user_score != "tbd" else None
+        except:
+            user_score = None
 
-    try:
-        critic_review_count = int(
-            soup.find("span", {"class": "count"}).find("a").text.split()[0]
-        )
-    except:
-        critic_review_count = 0
+        try:
+            critic_review_count = int(
+                soup.find("span", {"class": "count"}).find("a").text.split()[0]
+            )
+        except:
+            critic_review_count = 0
 
-    try:
-        user_rating_count = int(
-            soup.find_all("div", {"class": "summary"})[1]
-            .find("a")
-            .text.strip()
-            .split()[0]
-        )
-    except:
-        user_rating_count = 0
+        try:
+            user_rating_count = int(
+                soup.find_all("div", {"class": "summary"})[1]
+                .find("a")
+                .text.strip()
+                .split()[0]
+            )
+        except:
+            user_rating_count = 0
 
-    game_data = {
-        "Name": data.get("name"),
-        "Release Date": datetime.strptime(
-            data.get("datePublished"), "%B %d, %Y"
-        ).strftime("%Y-%m-%d"),
-        "Maturity Rating": data.get("contentRating", "Unspecified").replace(
-            "ESRB ", ""
-        ),
-        "Genre": ", ".join(data.get("genre", [])),
-        "Platform": data.get("gamePlatform"),
-        "Developer": soup.select_one(".developer a").text,
-        "Publisher": ", ".join([x["name"] for x in data["publisher"]]),
-        "Meta Score": int(data["aggregateRating"]["ratingValue"])
-        if "aggregateRating" in data
-        else None,
-        "Critic Reviews Count": critic_review_count,
-        "User Score": user_score,
-        "User Rating Count": user_rating_count,
-        "Summary": data.get("description"),
-        "Image": data["image"],
-    }
-    data_list.append(game_data)
-
-    try:
-        retry_late_list.remove(link)
-    except ValueError:
-        pass
+        try:
+            developer = soup.select_one(".developer a").text
+        except:
+            devloper = None
+        game_data = {
+            "Name": data.get("name"),
+            "Release Date": datetime.strptime(
+                data.get("datePublished"), "%B %d, %Y"
+            ).strftime("%Y-%m-%d"),
+            "Maturity Rating": data.get("contentRating", "Unspecified").replace(
+                "ESRB ", ""
+            ),
+            "Genre": ", ".join(data.get("genre", [])),
+            "Platform": data.get("gamePlatform"),
+            "Developer": developer,
+            "Publisher": ", ".join([x["name"] for x in data["publisher"]]),
+            "Meta Score": int(data["aggregateRating"]["ratingValue"])
+            if "aggregateRating" in data
+            else None,
+            "Critic Reviews Count": critic_review_count,
+            "User Score": user_score,
+            "User Rating Count": user_rating_count,
+            "Summary": data.get("description"),
+            "Image": data["image"],
+        }
+        data_list.append(game_data)
+    except BaseException as e:
+        print(f"On game link {link}, Error : {e}")
+        exception_list.append(f"On game link {link}, Error : {e}")
 
 
 def main(console: str) -> None:
@@ -196,30 +200,15 @@ def main(console: str) -> None:
     """
     data_list = []
     exception_list = []
-    failed_links_list = []
-    retry_late_list = []
     game_list = read_txt(console)
 
     for game in game_list:
         print(f"processing {game} data.")
-        if game in retry_late_list:
-            scrape_game_data(
-                game, data_list, exception_list, retry_late_list, failed_links_list
-            )
-            continue
-        elif game in failed_links_list:
-            continue
-        else:
-            scrape_game_data(
-                game, data_list, exception_list, retry_late_list, failed_links_list
-            )
-            if game in exception_list:
-                retry_late_list.append(game)
+        scrape_game_data(game, data_list, exception_list)
 
     df1 = pd.DataFrame.from_dict(data_list)
     df1 = add_gamepass_status(df1)
     df1.to_parquet(f"/etc/scraped_data/{console}-games.parquet")
-    print(failed_links_list)
 
 
 if __name__ == "__main__":
