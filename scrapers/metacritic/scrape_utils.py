3d4ae04f0e585b585fd0571ba1d6cb65a1fc3dd3
Liftingthedata <liftingthedata@gmail.com>
2023-06-20 10:22:59 +0200

    16d02a047e

--- scrapers/metacritic/scrape_utils.py
+++ scrapers/metacritic/scrape_utils.py
@@ -117,4 +117,4 @@ def get_game_urls(link: str, pages: int) -> Generator[str, None, None]:
         soup = get_soup(link + f"&page={page}")
         title_elements = soup.find_all("a", class_="title")
         for elem in title_elements:
-            yield f"{elem.get('href')}"
+            yield f"{elem.get('href').rsplit('/', 1)[-1]}"
