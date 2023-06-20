249a40875cf6ef7ff97a5cf0893da10b1791073e
Liftingthedata <liftingthedata@gmail.com>
2023-06-20 21:15:38 +0200

    62ea6d580b

--- scrapers/metacritic/scrape_games_data.py
+++ scrapers/metacritic/scrape_games_data.py
@@ -230,3 +230,4 @@ if __name__ == "__main__":
     df1 = pd.DataFrame.from_records(game_data)
     df1 = add_gamepass_status(df1)
     df1.to_parquet(f"{local_path}{console}-games.parquet")
+    df1.head()
