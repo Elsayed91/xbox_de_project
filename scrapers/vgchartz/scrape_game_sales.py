80678654edcb9c002c9b4e9b6c4af8dceabfc44b
Liftingthedata <liftingthedata@gmail.com>
2023-06-28 15:29:31 +0200

    0b66db6bc0

--- scrapers/vgchartz/scrape_game_sales.py
+++ scrapers/vgchartz/scrape_game_sales.py
@@ -107,7 +107,7 @@ def clean_data(df: pd.DataFrame) -> pd.DataFrame:
         {"XS": "Xbox Series X", "XOne": "Xbox One", "X360": "Xbox 360", "XB": "Xbox"}
     )
 
-    df["Release Date"] = pd.to_datetime(df["ReleaseDate"], format="%dth %b %y")
+    df["Release Date"] = pd.to_datetime(df["Release Date"], format="%dth %b %y")
     df["Last Update"] = pd.to_datetime(df["Last Update"], format="%dth %b %y")
     df["Release Year"] = df["Release Date"].dt.year
 
